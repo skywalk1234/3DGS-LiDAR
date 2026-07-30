704401336548709becbd4edf8a8f8604cd763546
skywalk1234 <skywalk1234@users.noreply.github.com>
2026-07-30 16:35:31 +0800

    先overfit=1，在一个batch上跑很多轮看看能不能快速收敛。inference的pred_valid改成gt_valid用GT 的掩码来过滤，看看蓝色柱子问题能否解决

--- ReconDrive/ReconDrive/scripts/trainer.py
+++ ReconDrive/ReconDrive/scripts/trainer.py
@@ -126,7 +126,7 @@ def main():
     trainer = pl.Trainer(
         max_epochs=main_cfg.get('train_epoch', 50),
         accelerator="gpu",
-        devices=main_cfg['devices'],
+        devices=1,  # overfit_batches 模式下只用单卡
         precision="32-true",
         gradient_clip_algorithm="norm",
         accumulate_grad_batches=8,
@@ -136,9 +136,10 @@ def main():
         log_every_n_steps=1,
         enable_progress_bar=True,
         enable_model_summary=True,
-        strategy='ddp_find_unused_parameters_true',
+        strategy='auto',
         profiler="simple",
         logger=logger,
+        overfit_batches=1,  # 只用一个 batch 反复训练，验证代码正确性
     )
 
     torch.use_deterministic_algorithms(mode=True,warn_only=True)
